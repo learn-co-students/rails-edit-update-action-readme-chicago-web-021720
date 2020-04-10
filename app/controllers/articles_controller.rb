c86e62e3b64a880d4ef8d34a90aaf0c242972b89
owenabbott <ocabbott@gmail.com>
2020-04-10 12:18:38 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,22 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
-    @article.save
+    article_params = params.require(:article).permit(:title, :description)
+    @article = Article.create(article_params)
+    # @article = Article.new
+    # @article.title = params[:title]
+    # @article.description = params[:description]
+    # @article.save
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+post 'articles/new', to: 'articles#create'
+get 'articles/new', to: 'articles#new'
+get 'articles/:id', to: 'articles#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'articles/:id/edit', to: 'articles#edit'
+patch 'articles/:id/edit', to: 'articles#update'
+
+
+
 end
